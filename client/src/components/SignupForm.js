1bd7466595d9cb1f92b5b5ce59c8c9a8cdc34134
Jess Varghese <jessvarghese@gmail.com>
2022-02-25 19:45:02 -0500

    updated signup file

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 
@@ -11,6 +11,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,19 +30,21 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await ADD_USER(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  
 
     setUserFormData({
       username: '',
@@ -104,6 +108,7 @@ const SignupForm = () => {
         </Button>
       </Form>
     </>
+    
   );
 };
 
